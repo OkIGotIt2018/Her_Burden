d92a7af3cb6454c19fa5cc38013b4d9b246d06fa
Kyler Akers <kylera14@gmail.com>
2020-09-13 18:00:45 -0500

    Converted to .NET Standard 2.0.

---
+++ Her_Burden/SizeScripts/BodySizeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace CustomItemPlugin
+{
+    internal class BodySizeScript : MonoBehaviour
+    {
+        private static int globalIndexTick = 0;
+        private int personalIndex;
+
+        private void OnEnable()
+        {
+            personalIndex = globalIndexTick;
+            globalIndexTick++;
+        }
+
+        //This is passed the stack count from Inventory, and hands it to the PrefabSizeScript
+        internal void UpdateStacks(int newStacks)
+        {
+            SizeHandoffManager.prefabSizeScripts.ElementAt(personalIndex).UpdateStacks(newStacks);
+        }
+    }
+}
---
+++ Her_Burden/SizeScripts/PrefabSizeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace CustomItemPlugin
+{
+    internal class PrefabSizeScript : MonoBehaviour
+    {
+        private static Vector3 originalScale = new Vector3(1f, 1f, 1f);
+        //These could be set in a config if you wanted to
+        internal static float maxSizeMultiplier = 10f, stackSizeMultiplier = 0.5f;
+        private Transform thisTransform;
+
+        private void OnEnable()
+        {
+            thisTransform = gameObject.transform;
+            SizeHandoffManager.prefabSizeScripts.Add(this);
+        }
+
+        //This handles all of the item size changes, and is called by BodySizeScript
+        internal void UpdateStacks(int newStacks)
+        {
+            float testSizeMultiplier = 1 + (newStacks * stackSizeMultiplier);
+            if(testSizeMultiplier <= maxSizeMultiplier)
+            {
+                thisTransform.localScale = originalScale * testSizeMultiplier;
+            }
+            else
+            {
+                thisTransform.localScale = originalScale * maxSizeMultiplier;
+            }
+        }
+    }
+}
