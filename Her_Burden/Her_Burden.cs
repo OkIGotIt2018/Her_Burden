75bb4516296639c4b826fa80748d258a2885318b
OkIGotIt <okigotit2018@gmail.com>
2021-11-29 01:03:48 -0700

    1.5.1

--- Her_Burden/Her_Burden.cs
+++ Her_Burden/Her_Burden.cs
@@ -2,7 +2,7 @@
 
 namespace Her_Burden
 {
-    [BepInPlugin("com.OkIgotIt.Her_Burden", "Her_Burden", "1.5.0")]
+    [BepInPlugin("com.OkIgotIt.Her_Burden", "Her_Burden", "1.5.1")]
 
     public class Her_Burden : BaseUnityPlugin
     {
